0fafe2ac6630d163e087639c695f4bc1ecb3f8b2
ISAWASHUN <syun20000616@icloud.com>
2024-03-03 21:58:41 +0900

    Aboutの作成

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
+import { FaTwitter, FaGithub } from "react-icons/fa";
 
 const About = () => {
   return (
-    <div className="max-w-4xl mx-auto mt-10 px-4">
-      <h1 className="text-4xl text-center font-bold mb-6">こんにちは、私の名前は山田太郎です</h1>
-      <p className="text-base text-gray-700 mb-4">
-        私は東京出身のフロントエンド開発者で、ReactやJavaScriptに情熱を持っています。
-        新しいテクノロジーを学ぶことに興味があり、ユーザーフレンドリーなウェブサイトを作成することを目指しています。
-      </p>
-      <p className="text-base text-gray-700 mb-4">
-        私はコーディングだけでなく、デザインにも興味があり、ユーザー体験を向上させるための
-        インタラクティブな機能を考案することに情熱を注いでいます。趣味は読書と旅行で、
-        新しい文化を体験することでインスピレーションを得ています。
-      </p>
-      <p className="text-base text-gray-700 mb-6">
-        技術スタックには、HTML、CSS、JavaScript、React、Redux、Node.jsなどがあります。
-        プロジェクトに貢献し、チームで協力して働くことを楽しみにしています。
-      </p>
-      <p className="text-base text-gray-700">ご連絡は、[あなたのメールアドレス]までお気軽にどうぞ。</p>
-    </div>
+    <>
+      <h1 id='about' className='text-7xl mt-64 text-center'>About Me</h1>
+      <div className="flex flex-col md:flex-row max-w-4xl mx-auto mt-16 px-4 md:space-x-8">
+        <div className='mb-10 md:mb-0 flex-shrink-0'>
+          <img src="/profile_logo.png" alt="ロゴ" className='h-auto w-52 mx-auto'/>
+        </div>
+        <div className="flex-grow">
+          <p className="text-base text-gray-700 mb-4">
+            私は大阪出身で、東京都内で医療系企業でエンジニアとして働いています。
+            サービスを成長させることに情熱を注いでおり、ユーザーに価値を提供することに喜びを感じています。
+          </p>
+          <p className="text-base text-gray-700 mb-4">
+            趣味は読書とスポーツ観戦で、阪神タイガースとリバプールFCのファンです。
+          </p>
+          <p className="text-base text-gray-700 mb-6">
+            プロジェクトに貢献し、チームで協力して働くことを生き甲斐にしています。
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center space-x-6 mt-6">
+        <a href="https://twitter.com/dosukoi_org" target="_blank" rel="noopener noreferrer" className='text-blue-500'>
+          <FaTwitter className="w-10 h-10" />
+        </a>
+        <a href="https://github.com/ISAWASHUN" target="_blank" rel="noopener noreferrer" className='text-black'>
+          <FaGithub className="w-10 h-10" />
+        </a>
+        <a href="https://zenn.dev/isawa" target="_blank" rel="noopener noreferrer" className=''>
+          <img src="/zenn.jpeg" alt="zenn" className='h-auto w-32'/>
+        </a>
+      </div>
+    </>
   )
 }
 
