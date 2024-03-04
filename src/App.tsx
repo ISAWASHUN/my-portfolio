0c7dd3054bef1515ab6f2b80b01a4affe686d019
ISAWASHUN <syun20000616@icloud.com>
2024-03-04 22:08:17 +0900

    Skillsの追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/About';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Skills from './components/Skills';
 import Work from './components/Work';
 
 
@@ -12,6 +13,7 @@ function App() {
       <Hero/>
       <About/>
       <Work/>
+      <Skills/>
     </div>
   );
 }
