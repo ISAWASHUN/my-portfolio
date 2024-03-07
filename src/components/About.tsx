04e8af659cb5a20e2615c8bb048585da98389ae6
ISAWASHUN <syun20000616@icloud.com>
2024-03-08 00:01:15 +0900

    Framer motionの追加

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,27 +1,67 @@
+import { motion } from 'framer-motion';
 import { FaTwitter, FaGithub } from "react-icons/fa";
 
 const About = () => {
   return (
     <>
-      <h1 id='about' className='text-7xl mt-64 text-center'>About Me</h1>
-      <div className="flex flex-col md:flex-row max-w-4xl mx-auto mt-16 px-4 md:space-x-8">
+      <motion.h1
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 1 }}
+        transition={{ duration: 1 }}
+        id='about' className='text-7xl mt-64 font-bold text-center'
+      >
+        About Me
+      </motion.h1>
+      <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="flex flex-col md:flex-row max-w-4xl mx-auto mt-16 px-4 md:space-x-8"
+      >
         <div className='mb-10 md:mb-0 flex-shrink-0'>
           <img src="/profile_logo.png" alt="ロゴ" className='h-auto w-52 mx-auto'/>
         </div>
         <div className="flex-grow">
-          <p className="text-base text-gray-700 mb-4">
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.5 }}
+            className="text-base mb-4"
+          >
             私は大阪出身で、東京都内で医療系企業でエンジニアとして働いています。
             サービスを成長させることに情熱を注いでおり、ユーザーに価値を提供することに喜びを感じています。
-          </p>
-          <p className="text-base text-gray-700 mb-4">
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.7 }}
+            className="text-base mb-4"
+          >
             趣味は読書とスポーツ観戦で、阪神タイガースとリバプールFCのファンです。
-          </p>
-          <p className="text-base text-gray-700 mb-6">
-            プロジェクトに貢献し、チームで協力して働くことを生き甲斐にしています。
-          </p>
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.9 }}
+            className="text-base mb-6"
+          >
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.1 }}
+            className="text-base"
+          >
+            私は新しい技術を学ぶことが好きで、最近はRustに興味があります。
+          </motion.p>
         </div>
-      </div>
-      <div className="flex justify-center items-center space-x-7 mt-6">
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1 }}
+        className="flex justify-center items-center space-x-7 mt-6"
+      >
         <a href="https://twitter.com/dosukoi_org" target="_blank" rel="noopener noreferrer" className='text-blue-500'>
           <FaTwitter className="w-10 h-10" />
         </a>
@@ -31,7 +71,10 @@ const About = () => {
         <a href="https://zenn.dev/isawa" target="_blank" rel="noopener noreferrer">
           <img src="/zenn.jpeg" alt="zenn" className='h-auto w-32'/>
         </a>
-      </div>
+        <a href="https://qiita.com/dosukoi_org" target="_blank" rel="noopener noreferrer">
+          <img src="/qiita.png" alt="qiita" className='h-auto w-16'/>
+        </a>
+      </motion.div>
     </>
   )
 }
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion'; // Framer Motion をインポート
 import { WorksData } from '../data/data'
 import { FaGithub } from 'react-icons/fa'
 import { FiExternalLink } from "react-icons/fi";
@@ -17,20 +18,30 @@ const Work = () => {
       <section className="p-4">
         <div className="container mt-32 mx-auto">
           <h2 className="text-7xl text-center font-bold">Work</h2>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-1">
             {WorksData.map((item: WorkerData) => (
-              <div key={item.id} className="bg-gray-100 p-4 rounded-lg flex flex-col mx-auto mt-16">
-                <div className='mb-4'> {/* mb-4 provides some margin below the image */}
+              <motion.div
+                key={item.id}
+                initial={{ opacity: 0, scale: 0.75 }} // 初期状態
+                animate={{ opacity: 1, scale: 1 }} // アニメーション後の状態
+                transition={{ duration: 0.5 }} // アニメーションの時間
+                className="bg-gray-300 w-80 p-4 rounded-lg flex flex-col mx-auto mt-16"
+              >
+                <div className='mb-4'>
                   <img src={item.img} alt={item.title} className="h-64 w-full object-cover rounded-lg" />
                 </div>
                 <div className='flex-grow'>
-                  <h3 className="text-2xl font-bold mb-2">{item.title}</h3> {/* mb-2 provides some margin below the title */}
-                  <div className="mb-4"> {/* mb-4 provides some margin below the links */}
-                    <a href={item.link} target='_blank' className="text-blue-500" rel="noreferrer"><FiExternalLink className='w-10 h-10 inline'/></a>
-                    <a href={item.github} className="ml-4"><FaGithub className="w-10 h-10 inline" /></a> {/* ml-4 adds margin to the left of the GitHub icon */}
+                  <h3 className="text-2xl text-center font-bold mb-2">{item.title}</h3>
+                  <div className="mb-4 flex justify-center">
+                    <a href={item.link} target='_blank' className="text-blue-500" rel="noreferrer">
+                      <FiExternalLink className='w-10 h-10 inline'/>
+                    </a>
+                    <a href={item.github} className="ml-4">
+                      <FaGithub className="w-10 h-10 inline" />
+                    </a>
                   </div>
                 </div>
-              </div>
+              </motion.div>
             ))}
           </div>
         </div>
