052beee08b36d257be11923d9297785bbb75f278
ISAWASHUN <syun20000616@icloud.com>
2024-03-08 17:50:24 +0900

    componentの追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
+import TimeLine from './components/TimeLine';
 import Work from './components/Work';
-
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
       <About/>
       <Work/>
       <Skills/>
+      <TimeLine/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
