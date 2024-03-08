e0ee3eed665b8c5f0ed49896a4fd369b979c7e30
ISAWASHUN <syun20000616@icloud.com>
2024-03-08 17:51:22 +0900

    skillsの完成

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { motion } from 'framer-motion';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
 import { DevopsData, backendData, frontendData, optionsFrontend, optionsBackend, optionsDevops } from '../data/data';
@@ -8,8 +8,15 @@ ChartJS.register(ArcElement, Tooltip, Legend, Title);
 const Skills = () => {
   return (
     <div className='mt-12 md:mt-48 px-4 md:px-0'>
-      <h1 className='text-7xl mt-32 text-center mb-16 font-bold '>My Skills</h1>
-      <div className='flex flex-col md:flex-row items-center justify-center gap-12 md:gap-12'>
+      <motion.h1
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 1 }}
+        transition={{ duration: 1 }}
+        id='about' className='text-7xl mt-32 text-center font-bold '
+      >
+        My Skills
+      </motion.h1>
+      <div className='flex flex-col md:flex-row mt-24 items-center justify-center gap-12 md:gap-12'>
         <div className="text-center">
           <div className="mb-4 w-100 h-100 inline-block">
             <Pie data={frontendData} options={optionsFrontend} />
